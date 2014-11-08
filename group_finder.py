92393312dcbdc37ee4a55e5b253d830f50868ad4
Duncan Campbell <duncan.alan.campbell@gmail.com>
2014-11-07 21:52:06 -0500

    fixed some bugs

--- group_finder.py
+++ group_finder.py
@@ -21,8 +21,8 @@ def fof_link(mock, b_para=0.75, b_perp=0.14, los=np.array([0,0,1]), period=np.ar
     tree = cKDTree(data)
     
     ngal = len(mock)/(250.0**3.0)
-    b_perp = 0.14*ngal**(-1.0/3.0)
-    b_para = 0.75*ngal**(-1.0/3.0)
+    b_perp = b_perp*ngal**(-1.0/3.0)
+    b_para = b_para*ngal**(-1.0/3.0)
     b = max(b_perp,b_para)
 
     sparse_d_matrix = tree.sparse_distance_matrix_custom(\
@@ -46,8 +46,8 @@ def fof_conditional_link(mock, w1, w2, b_para=0.75, b_perp=0.14,los=np.array([0,
     tree = cKDTree(data)
     
     ngal = len(mock)/(250.0**3.0)
-    b_perp = 0.14*ngal**(-1.0/3.0)
-    b_para = 0.75*ngal**(-1.0/3.0)
+    b_perp = b_perp*ngal**(-1.0/3.0)
+    b_para = b_para*ngal**(-1.0/3.0)
     b = max(b_perp,b_para)
 
     sparse_d_matrix = tree.sparse_distance_matrix_custom(tree, b, period=period, los=los,\
@@ -58,44 +58,71 @@ def fof_conditional_link(mock, w1, w2, b_para=0.75, b_perp=0.14,los=np.array([0,
     return _scipy_to_igraph(csr_sparse_d_matrix)
 
 
+def min_spanning_groups(mock):
+    group_IDs = get_gal_host_halo_IDs(mock)
+    p_g = fof_conditional_link(mock, group_IDs, group_IDs, b_para=5.0, b_perp=1.5)
+    edges = p_g.es()
+    e_lenghts = edges.get_attribute_values('weight')
+    result = p_g.spanning_tree(weights = e_lenghts)
+    return result
+
+
 def gal_group_IDs(g):
     clusters = g.clusters()
     group_IDs = clusters.membership
-    
-    return group_IDs
+    return np.array(group_IDs)
 
 
 def group_IDs(g):
     clusters = g.clusters()
     group_IDs = np.unique(clusters.membership)
-    
-    return group_IDs
+    return np.array(group_IDs)
 
 
 def group_multiplicity(g):
     clusters = g.clusters()
     m = clusters.sizes()
-    
-    return m
+    return np.array(m)
+
+
+def gal_group_multiplicity(g):
+    clusters = g.clusters()
+    m = np.array(clusters.sizes())
+    gID = gal_group_IDs(g)
+    return m[gID]
 
 
 def n_groups(g):
     clusters = g.clusters()
-    n = clusters.n
-    
+    n = len(clusters.sizes())
     return n
 
 
 def gal_degree(g):
     degree = g.degree()
-    
-    return degree
+    return np.array(degree)
 
 
 def gal_betweenness(g):
     result = g.betweenness()
-    
-    return result
+    return np.array(result)
+
+
+def get_edges(g):
+    edges = g.get_edgelist()
+    return np.array(edges)
+
+
+def get_edge_lengths(g):
+    edges = g.es()
+    lens = edges.get_attribute_values('weight')
+    return np.array(lens)
+
+
+def get_gal_host_halo_IDs(mock):
+    import build_perfect_groups
+    group_IDs = build_perfect_groups.get_group_IDs(mock['ID_host'], mock['ID_halo'])
+    return np.array(group_IDs)
 
 
 def open_mock():
@@ -114,8 +141,7 @@ def open_mock():
 
 def _scipy_to_igraph(matrix, directed=False):
     sources, targets = matrix.nonzero()
-    weights = matrix[sources, targets]
-    return igraph.Graph(zip(sources, targets), n=matrix.shape[0],\
-                        directed=directed, edge_attrs={'weight': weights})
+    weights = matrix[sources, targets].tolist()[0]
+    return igraph.Graph(zip(sources, targets), n=matrix.shape[0], directed=directed, edge_attrs={'weight': weights})
     
     
